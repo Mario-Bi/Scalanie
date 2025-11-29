172b16065a9d333eaf00955007eacc8c51e57078
Marusz <maruszmalaga001@gmail.com>
2025-11-29 20:42:17 +0100

    dodanie maina

--- Scalanie.cpp
+++ Scalanie.cpp
@@ -1,20 +1,26 @@
-﻿// Scalanie.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
-
-#include <iostream>
+﻿#include <iostream>
+#include <vector>
+#include "MergeSort.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    MergeSort<int> sorterInt;
+    MergeSort<double> sorterDouble;
+
+    std::vector<int> arrayInt = { 5, 3, 8, 1, 2 };
+    std::vector<double> arrayDouble = { 3.1, 2.4, 5.5, 1.0 };
+
+    sorterInt.sort(arrayInt);
+    sorterDouble.sort(arrayDouble);
+
+    std::cout << "Posortowana tablica int: ";
+    for (int x : arrayInt)
+        std::cout << x << " ";
+
+    std::cout << "\nPosortowana tablica double: ";
+    for (double x : arrayDouble)
+        std::cout << x << " ";
+
+    std::cout << std::endl;
+    return 0;
 }
-
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
